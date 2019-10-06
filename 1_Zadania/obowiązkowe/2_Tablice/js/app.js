ca5e6109751be4be1fba58f00cd03dc134cf7c67
Gorzelany Artur <artur.gorzelany@microsoft.wsei.edu.pl>
2019-10-06 13:48:17 +0200

    zadanie 0,1 z tablic

---
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -0,0 +1,31 @@
+/// ZADANIE 0
+function distFromAvarage(tab) {
+    let sum = 0;
+    for(let x of tab){
+        sum= sum + x;
+    }
+
+    let y = sum/tab.length;
+
+    let tab2= [];
+    for(let v of tab){
+        tab2.push(y - v)
+    }
+    console.log(tab2);
+}
+
+distFromAvarage([1,2,3,4,5,6,7])
+
+
+
+/// ZADANIE 1
+const fruits = ['banana', 'strawberry', 'ananas'];
+
+console.log(fruits[0]);
+console.log(fruits[fruits.length - 1]);
+
+for(var i = 0; i<fruits.length; i++){
+    console.log(fruits[i])
+}
+
+
